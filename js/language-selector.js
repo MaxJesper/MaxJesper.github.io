257899fb7710b808258848f3d5db6b2ae7fd9e22
Jesper Tordsson <jesper.tordsson@gmail.com>
2026-09-14 18:31:26 +0200

    Lägg till Kinyarwanda i språkväljaren och 9 begreppslistor

--- js/language-selector.js
+++ js/language-selector.js
@@ -22,7 +22,8 @@
     { code: 'en-GB', label: '🇬🇧 Engelska (English)',         prefix: 'en' },
     { code: 'bs',    label: '🇧🇦 Bosniska (Bosanski)',        prefix: 'bs' },
     { code: 'es-ES', label: '🇪🇸 Spanska (Español)',         prefix: 'es' },
-    { code: 'ur-PK', label: '🇵🇰 Urdu (اردو)',              prefix: 'ur' }
+    { code: 'ur-PK', label: '🇵🇰 Urdu (اردو)',              prefix: 'ur' },
+    { code: 'rw',    label: '🇷🇼 Kinyarwanda (Ikinyarwanda)', prefix: 'rw' }
   ];
 
   var CSS = [
@@ -43,7 +44,7 @@
   // Mappar TTS-språkkod → filprefix för begrepp-JSON (t.ex. 'ar-SA' → 'ar')
   var LANG_PREFIX = {
     'sv-SE': 'sv', 'ar-SA': 'ar', 'so': 'so', 'fa': 'fa',
-    'am-ET': 'am', 'ps-AF': 'ps', 'pl-PL': 'pl', 'en-GB': 'en', 'bs': 'bs', 'es-ES': 'es', 'ur-PK': 'ur'
+    'am-ET': 'am', 'ps-AF': 'ps', 'pl-PL': 'pl', 'en-GB': 'en', 'bs': 'bs', 'es-ES': 'es', 'ur-PK': 'ur', 'rw': 'rw'
   };
 
   // Cache: {basePath+prefix → data[]}
